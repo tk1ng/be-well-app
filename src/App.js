6600a87d5a6d3d09b964ca4219b9922380fce626
Taryn King <marshaeking@gmail.com>
2023-02-17 13:30:54 -0600

    chore: clean up some unused code

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <p>
-          Wellness Tracker
-        </p>
-      </header> */}
       <Auth />
     </div>
   );
