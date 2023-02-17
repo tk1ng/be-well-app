884810c30468f08ea1eec48d8c30c6697aa8f62d
Taryn King <marshaeking@gmail.com>
2023-02-17 14:53:55 -0600

    refactor: move general styles to index.css

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './navBar/Navbar';
-import styles from './RootLayout.module.css';
 
 const RootLayout = () => {
     return (
---
+++ src/views/Error.js
@@ -0,0 +1,10 @@
+const Error = () => {
+    return (
+        <main>
+            <h1>An Error Occurred</h1>
+            <p>Could not find this page</p>
+        </main>
+    );
+}
+
+export default Error;
