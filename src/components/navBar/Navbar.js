7618e3fbcb38b9423dfe80f63ce48b48c86333fe
Taryn King <marshaeking@gmail.com>
2023-02-17 15:01:41 -0600

    refactor: opt to use NavLink over Link to allow for active styling

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -1,12 +1,16 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <header>
             <nav>
                 <ul>
-                    <li>Logs</li>
-                    <li>Sign Out</li>
+                    <li>
+                        <NavLink>Logs</NavLink>
+                    </li>
+                    <li>
+                        <NavLink>Sign Out</NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
