4346366c756f733f9a946aa326c8a37282fe8cfd
Taryn King <marshaeking@gmail.com>
2023-02-20 15:48:58 -0600

    refactor: correct import statements due to file name change

--- src/server/models/entry.js
+++ src/server/models/entry.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 
-const sequelize = require('../util/database');
+const sequelize = require('../utils/database');
 
 const Entry = sequelize.define('entry', {
     id: {
--- src/server/server.js
+++ src/server/server.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 
-const { REACT_APP_SERVER_PORT } = process.env;
-
-const sequelize = require('./util/database');
+const sequelize = require('./utils/database');
 
 const Entry = require('./models/entry');
 const User = require('./models/user');
 const Wellness_score = require('./models/wellness');
 const { register, login } = require('./controllers/auth');
 const { isAuthenticated } = require('./middleware/isAuthenticated');
+const { getEntries } = require('./controllers/mealEntries');
 
+const { REACT_APP_SERVER_PORT } = process.env;
 const app = express();
 
 app.use(cors())
@@ -23,11 +23,12 @@ app.post('/login', login);
 
 // TODO: Set up routes for retrieving data:
 // getting all logs ordered chronologically
+app.get('/entries', isAuthenticated, getEntries)
+
 // getting logs for a single day
 // getting just one entry(maybe this can be a task just for frontend?)
 // getting wellness scores of all time
 // getting wellness scores for one day(presented using chartjs?)
-// app.get('/logs', getLogs)
 // app.get('/logs/:id', getSingleLog)
 // app.get('/scores', getScores)
 
