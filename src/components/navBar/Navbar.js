258c912a52100889f42366b0b6f337070e6fd4b2
Taryn King <marshaeking@gmail.com>
2023-02-25 17:15:40 -0600

    styles: style navbar for desktop

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -1,5 +1,7 @@
 import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { IconContext } from 'react-icons';
+import { RiLogoutBoxRLine } from 'react-icons/ri';
 import AuthContext from '../../context/authContext';
 import styles from './Navbar.module.css';
 
@@ -11,27 +13,29 @@ const Navbar = () => {
     }
 
     return (
-        <header>
-            <nav>
-                <ul className={styles.navBar}>
-                    <li>
-                        <NavLink to='/' className={({ isActive }) => isActive ? styles.active : undefined} end>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/meal-tracker' className={({ isActive }) => isActive ? styles.active : undefined}>Entries</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/wellness' className={({ isActive }) => isActive ? styles.active : undefined}>Wellness</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className={({ isActive }) => isActive ? styles.active : undefined} to='/profile'>Profile</NavLink>
-                    </li>
-                    <li>
-                        <button className={styles.signOutBtn} onClick={handleSignOut}>Sign Out</button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <IconContext.Provider value={{ size: 20 }}>
+            <header className={styles.header}>
+                <nav>
+                    <ul className={styles.navBar}>
+                        <li>
+                            <NavLink to='/' className={({ isActive }) => isActive ? styles.active : undefined} end>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/meal-tracker' className={({ isActive }) => isActive ? styles.active : undefined}>Entries</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/wellness' className={({ isActive }) => isActive ? styles.active : undefined}>Wellness</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className={({ isActive }) => isActive ? styles.active : undefined} to='/profile'>Profile</NavLink>
+                        </li>
+                        <li>
+                            <button className={styles.signOutBtn} onClick={handleSignOut}><RiLogoutBoxRLine /></button>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </IconContext.Provider>
     );
 }
 
